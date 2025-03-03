b21e71b66986657090dd97876fee168eaeef26e1
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-03-03 20:19:26 +0800

    add Bit Operation In Cpp.

---
+++ Day-03/BitOperationIn.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    // bit wish operator <<
+    int a = 11;
+    cout << (a << 1) << endl;
+    // bit wish operator >>
+    int s = 2;
+    cout << (s >> 1) << endl;
+    // bitwish operator &
+    int as = 4;
+    cout << (as & as);
+    // bit wish operator |
+    // bit wish operator ^
+    // bit wish operator ~
+    return 0;
+}
