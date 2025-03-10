f3d858b98854cedc3c5ca6669f4bc6af21c07798
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-03-10 10:41:28 +0800

    add Condition Operator.

---
+++ Day-05/LogicOperator.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // if statement
+    int a, b;
+    cin >> a >> b;
+    if (a > b)
+    {
+        cout << "It's True.";
+    }
+    else if (a == b)
+    {
+        cout << "It's Equal.";
+       
+    }
+
+    else
+    {
+        cout << "It's False.";
+    }
+
+    return 0;
+}
