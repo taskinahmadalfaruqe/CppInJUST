20405cf6590dc77a62eb156f6fe3a480bda123ed
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-03-12 14:16:25 +0800

    add If Expression.

---
+++ Day-06/ifExpression.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 6;
+    if (5 + 6) // 5+6 is an expression
+    {
+        cout << "HI";
+    }
+
+    return 0;
+}
