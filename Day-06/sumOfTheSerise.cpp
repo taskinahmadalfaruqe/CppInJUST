9192baaeda974f181f5c989bd895cee1893b9b45
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-03-12 15:37:47 +0800

    Add Summetion of Array serrise.

---
+++ Day-06/sumOfTheSerise.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // double sum = 0;
+    // int a;
+    // cin >> a;
+    // int sign = -1;
+    // for (int i = 0; i < a; i++)
+    // {
+    //     sum += sign * 1 / (2 * i);
+    //     sign *= sign;
+    // }
+    // cout << sum;
+
+    cout << "Hello";
+
+    return 0;
+}
