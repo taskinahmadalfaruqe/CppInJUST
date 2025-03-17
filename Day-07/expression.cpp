c43303fab950601e7d984d0e7606602093e4c1d8
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-03-17 11:06:40 +0800

    Add Summetion of serise.

---
+++ Day-07/expression.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double sum = 0;
+    int number = 1;
+    while (number)
+    {
+        /* code */
+    }
+    
+    cout << sum;
+    return 0;
+}
