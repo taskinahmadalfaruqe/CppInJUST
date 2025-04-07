13fa793b30af7c58b952d766f69bf6fadc574d3c
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-04-07 11:41:59 +0800

    Add Parten.

--- Day-11/PartenZ.cpp
+++ Day-11/PartenZ.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (i == 0 || i == 4 || j == 4 - i)
+                cout << "X ";
+            else
+                cout << ". ";
+        }
+        cout << endl;
+    }
     return 0;
 }
