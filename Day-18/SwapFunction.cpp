96815e8571abea2538fe75f4754b388114b60652
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-04-30 15:35:09 +0800

    Add Some Code. With function.

---
+++ Day-18/SwapFunction.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Call by Value
+void swapFunction(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    swapFunction(&a, &b);
+    cout << a << " " << b;
+    return 0;
+}
