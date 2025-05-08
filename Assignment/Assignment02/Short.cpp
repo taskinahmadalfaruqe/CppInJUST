357f324efada40c0e2cd2b96412d710099f4c50f
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-05-08 23:19:19 +0800

    Add New Problem On Assignment,

---
+++ Assignment/Assignment02/Short.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Declea A Veriable to Seve this value/
+    float FloatNumber = 567.123;
+    char letter = 'F';
+    int IntNumber = 1000;
+    string message = "Welcome To Chine!";
+    double numbers[5] = {7, 2, 0.3, -1, 5.5};
+    double array01[4] = {'a', 'H', 72, 99};
+    string array02[3] = {"Smith", "Jack", "David"};
+    return 0;
+}
