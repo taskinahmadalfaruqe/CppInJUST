2fffdb6152f51488d8b52bca8b23e84b37551c40
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-05-13 01:25:45 +0800

    Add Odd Number Experement.

---
+++ Experiment/07_OddNumber.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // print all odd number 1 to 100
+    for (int i = 1; i < 101; i++)
+    {
+        if (i % 2 == 1)
+        {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
