3e8ddcb34d9862de733f11c8b9b10ba7184271c7
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-05-14 00:13:15 +0800

    Add Shape

---
+++ Experiment/10_Shape.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5; // Total number of rows
+
+    for (int i = 0; i < n; i++) {
+        // Print leading spaces
+        for (int space = 0; space < i; space++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int star = 0; star < n - i; star++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
