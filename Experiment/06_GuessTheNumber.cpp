b511a5aef86b10c2814b4f0b4d91ad12314f6352
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-05-14 22:57:44 +0800

    Add New Guess Problem.cpp

---
+++ Experiment/06_GuessTheNumber.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void GuessTheNumber(int randomNum)
+{
+    int userInput;
+
+    while (true)
+    {
+        cout << "Enter your guess (1 to 100): ";
+        cin >> userInput;
+
+        if (userInput < randomNum)
+        {
+            cout << "Too low. Try again.\n";
+        }
+        else if (userInput > randomNum)
+        {
+            cout << "Too high. Try again.\n";
+        }
+        else
+        {
+            cout << "Congratulations! You guessed the correct number.\n";
+            break;
+        }
+    }
+}
+
+int main()
+{
+    srand(time(0));
+    int randomNumber = rand() % 100 + 1;
+    GuessTheNumber(randomNumber);
+    return 0;
+}
